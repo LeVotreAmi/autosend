edbb72be3967c58c89825d8ae884c264e4fb5365
LeVotreAmi <levotreami@gmail.com>
2019-10-08 00:57:11 +0300

    Initial commit

---
+++ AutoSend/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace AutoSend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.files = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.topic = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.email,
+            this.files,
+            this.topic,
+            this.text});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Menu;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 426);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.DataGridView1_SelectionChanged);
+            // 
+            // email
+            // 
+            this.email.HeaderText = "E-mail";
+            this.email.Name = "email";
+            this.email.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // files
+            // 
+            this.files.HeaderText = "Files";
+            this.files.Name = "files";
+            // 
+            // topic
+            // 
+            this.topic.HeaderText = "Topic";
+            this.topic.Name = "topic";
+            // 
+            // text
+            // 
+            this.text.HeaderText = "Text";
+            this.text.Name = "text";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(461, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(392, 377);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(841, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(461, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(392, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.Color.Honeydew;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(415, 463);
+            this.panel1.TabIndex = 5;
+            this.panel1.Click += new System.EventHandler(this.Panel1_Click);
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel1_DragEnter);
+            // 
+            // panel2
+            // 
+            this.panel2.AllowDrop = true;
+            this.panel2.BackColor = System.Drawing.Color.Honeydew;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Location = new System.Drawing.Point(438, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(415, 463);
+            this.panel2.TabIndex = 6;
+            this.panel2.Click += new System.EventHandler(this.Panel2_Click);
+            this.panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.Panel2_DragDrop);
+            this.panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel2_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(865, 487);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "AutoSend";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn files;
+        private System.Windows.Forms.DataGridViewTextBoxColumn topic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn text;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ AutoSend/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinForms = System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace AutoSend
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string pathXls = "";
+        static string pathFolder = "";
+
+        public void FileReader()
+        {
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(@"" + pathXls);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+
+            int nInLastRow = ObjWorkSheet.Cells.Find("*", System.Reflection.Missing.Value,
+                System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious,
+                false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
+            int nInLastCol = ObjWorkSheet.Cells.Find("*", System.Reflection.Missing.Value,
+                System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                Excel.XlSearchOrder.xlByColumns, Excel.XlSearchDirection.xlPrevious,
+                false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Column;
+
+            string[,] list = new string[nInLastRow, nInLastCol];
+
+            for (int i = 0; i < nInLastRow; i++)
+            {
+                string[] tempArrStr = new string[nInLastCol];
+                for (int j = 0; j < nInLastCol; j++)
+                {
+                    string str = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString().Replace("\n", "\r\n");
+                    list[i, j] = str;
+                    tempArrStr[j] += list[i, j];
+
+                    if (j == nInLastCol - 1)
+                    {
+                        dataGridView1.Rows.Add(tempArrStr);
+                    }
+                }
+            }
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+            ObjWorkExcel.Quit();
+            GC.Collect();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            int kk = 0;
+            try
+            {
+                if (MessageBox.Show("Отправить письма уверен ли ты, что хочешь?", "Точна?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                    {
+                        Outlook.Application oApp = new Outlook.Application();
+                        Outlook.MailItem mail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+
+                        k++;
+                        kk = 0;
+                        for (;;)
+                        {
+                            kk = 1;
+                            mail.To = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                            kk = 3;
+                            mail.Subject = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                            kk = 4;
+                            mail.Body = dataGridView1.Rows[i].Cells[3].Value.ToString();
+                            kk = 2;
+                            string str = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                            string[] attachmentPaths = explode(",", str);
+                            for (int j = 0; j < attachmentPaths.Length; j++)
+                            {
+                                attachmentPaths[j] = @"" + pathFolder + "\\" + attachmentPaths[j] + ".pdf";
+                                mail.Attachments.Add(attachmentPaths[j], null, null, null);
+                            }
+                            mail.Importance = Outlook.OlImportance.olImportanceNormal;
+                            mail.Send();
+                            oApp = null;
+                            mail = null;
+                            break;
+                        }
+                    }
+                    MessageBox.Show("Гатова", "Ok", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Чо-то не то с " + k + " строкой, " + kk + " столбцом", "Ой", MessageBoxButtons.OK);
+            }
+        }
+
+        private void DataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                {
+                    textBox1.Text = row.Cells[3].Value.ToString();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+                {
+                    if (textBox1.Text != null)
+                    {
+                        cell.Value = textBox1.Text;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+                {
+                    textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private string[] explode(string separator, string source)
+        {
+            return source.Split(new string[] { separator }, StringSplitOptions.None);
+        }
+
+        private void Panel1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "Файлы xlsx|*.xlsx";
+            ofd.ShowDialog();
+
+            pathXls = ofd.FileName;
+            if (pathXls != "" && pathFolder != "")
+            {
+                FileReader();
+                panel1.Visible = false;
+                panel2.Visible = false;
+                dataGridView1.Visible = true;
+                textBox1.Visible = true;
+                button1.Visible = true;
+                button2.Visible = true;
+            }
+        }
+
+        private void Panel2_Click(object sender, EventArgs e)
+        {
+            WinForms.FolderBrowserDialog fbd = new WinForms.FolderBrowserDialog();
+            fbd.ShowDialog();
+
+            pathFolder = fbd.SelectedPath;
+            if (pathXls != "" && pathFolder != "")
+            {
+                FileReader();
+                panel1.Visible = false;
+                panel2.Visible = false;
+                dataGridView1.Visible = true;
+                textBox1.Visible = true;
+                button1.Visible = true;
+                button2.Visible = true;
+            }
+        }
+
+        private void Panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    string file = (string)files[0];
+
+                    pathXls = file;
+                }
+
+                if (pathXls != "" && pathFolder != "")
+                {
+                    FileReader();
+                    panel1.Visible = false;
+                    panel2.Visible = false;
+                    dataGridView1.Visible = true;
+                    textBox1.Visible = true;
+                    button1.Visible = true;
+                    button2.Visible = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    string file = (string)files[0];
+
+                    pathFolder = file;
+                }
+
+                if (pathXls != "" && pathFolder != "")
+                {
+                    FileReader();
+                    panel1.Visible = false;
+                    panel2.Visible = false;
+                    dataGridView1.Visible = true;
+                    textBox1.Visible = true;
+                    button1.Visible = true;
+                    button2.Visible = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void Panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+    }
+}
